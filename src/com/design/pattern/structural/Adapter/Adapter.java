19b9295017a9492e35422a1039596efb87a6535b
Charlie Lee <charle0911@gmail.com>
2020-02-07 14:03:47 +0800

    Added adapter pattern

---
+++ src/com/design/pattern/structural/Adapter/Adapter.java
@@ -0,0 +1,42 @@
+package com.design.pattern.structural.Adapter;
+
+interface Hero {
+    void attack();
+}
+
+class MyHero implements Hero {
+
+    @Override
+    public void attack() {
+        System.out.println("My hero attack");
+    }
+}
+
+class UnionHero {
+    public void touch() {
+        System.out.println("Union Hero touch");
+    }
+}
+
+class HeroAdapter implements Hero {
+
+    private UnionHero unionHero = null;
+
+    HeroAdapter(UnionHero unionHero) {
+        this.unionHero = unionHero;
+    }
+
+    @Override
+    public void attack() {
+        unionHero.touch();
+    }
+}
+
+public class Adapter {
+    public static void main(String[] args) {
+        Hero hero = new MyHero();
+        Hero unionHero = new HeroAdapter(new UnionHero());
+        hero.attack();
+        unionHero.attack();
+    }
+}
